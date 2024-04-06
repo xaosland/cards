e0f3c315ac48fbe90f094ff3db03f7d3782a61c2
xaosland <xaosland@mail.ru>
2024-04-06 12:28:25 +0400

    Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
